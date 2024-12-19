e1e9a12718ccb128b9894cb4f742cf59198f3659
kamlakant kumar <kumarkamlakant46@gmail.com>
2024-12-19 22:10:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Navigation from "./components/nav";
+import Hero from "./components/Hero";
+const App =()=>{
+    return <div>
+       <Navigation/>
+       <Hero/>
+    </div>
+};
+
+export default App;
